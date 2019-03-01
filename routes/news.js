8b40e4f7bd390743d513765d1a7ccc64206c57d4
lRAHULl <rahulpadmaraju16@gmail.com>
2019-03-01 13:18:08 +0530

    News done , auth pending

--- routes/news.js
+++ routes/news.js
@@ -9,19 +9,29 @@ router.get('/news', (req, res) => {
       res.sendStatus(error)
     } // Print the error if one occurred
     console.log('News statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    const obj = JSON.parse(body) // Print the HTML for the Google homepage.
-    var result = '';
+    const obj = JSON.parse(body)
+    var result = [];
     for (let i = 0; i < obj.articles.length; i++) {
-      result += obj.articles[i].title + '\n' + obj.articles[i].description + '\n\n\n\n';
+      // result += obj.articles[i].title + '\n' + obj.articles[i].description + '\n\n\n\n';
+      temp = {}
+      temp.title = obj.articles[i].title;
+      temp.description = obj.articles[i].description
+      temp.urlToImage = obj.articles[i].urlToImage
+      temp.url = obj.articles[i].url
+      result.push(temp)
     }
-    res.send(result)
+    res.render('news.hbs', {
+      result: result
+    })
+    // res.json(result)
   })
-
-  // var obj = JSON.parse(response)
-  // obj.articles[0].title
-  // obj.articles[0].description
-  // https://newsapi.org/v2/everything?sources=the-times-of-india&apiKey=066d9529cb5141b681d9b6c3a6363be5
 })
+
+// var obj = JSON.parse(response)
+// obj.articles[0].title
+// obj.articles[0].description
+// https://newsapi.org/v2/everything?sources=the-times-of-india&apiKey=066d9529cb5141b681d9b6c3a6363be5
+
 
 
 module.exports = router
