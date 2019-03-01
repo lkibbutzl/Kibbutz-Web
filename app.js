3b3656a4f1e4b283ea4cb32649df7a4e14e56bde
lRAHULl <rahulpadmaraju16@gmail.com>
2019-03-01 16:39:03 +0530

    Designed the news page

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.use(bodyParser.urlencoded({
 app.use(userRouter)
 app.use(newsRouter)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
-  console.log('Server listening at port 3000.')
+  console.log('Server listening at port ' + PORT)
 })
 
 
